dbcab7b56d89c0106dd025dabdbe13b3e2f7b6f5
ksharonroy <ksharonroy@gmail.com>
2018-08-19 19:51:58 +0530

    Aug 19, 2018

---
+++ file2.R
@@ -0,0 +1,12 @@
+mtcars
+git config --global user.email "ksharonroy@gmail.com"
+git config --global user.name "ksharonroy"
+women
+plot(women)
+install.packages('car')
+?women
+?mean
+x = 1:100
+y
+
+y = seq(1,100, by=2)
